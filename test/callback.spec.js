18034fbbac20ec281f6dcc491fcba737df1c3190
Christophe Thibaut <cthibauttof@gmail.com>
2022-05-31 19:31:03 +0200

    callback for a delayed calculation, with test

--- test/callback.spec.js
+++ test/callback.spec.js
@@ -1,11 +1,17 @@
 const delayedSquare = (x =>  {
-    return new Promise(resolve => setTimeout( () => { resolve(x*x) }, 5000))
+    return new Promise(resolve => setTimeout( () => { resolve(x*x) }, 2000))
 })
 
+
 describe('a delayed square function', () => {
-    it('looks like it should compare result after a 5 secs yet compares result immediately', () => {
+    it('it should compare result after a 2 secs', (done) => {
         delayedSquare(42).then(result => {
-            expect(result).toStrictEqual(1764)
+            try {
+                expect(result).toStrictEqual(1764)
+                done()
+            } catch(error) {
+                done(error)
+            }
         })
     })
 })
