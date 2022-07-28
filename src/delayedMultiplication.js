15b8b021d588373bdf3483e48d031fac6f03edc8
Christophe Thibaut <cthibauttof@gmail.com>
2022-07-28 10:15:26 +0200

    remove sources that no longer relevant to the kata

--- src/delayedMultiplication.js
+++
@@ -1,8 +0,0 @@
-const delayed_multiplication = (x, y, callback) => {
-    const result = x * y;
-    setTimeout(() => {
-        callback(result);
-    }, 1000)
-};
-
-module.exports = delayed_multiplication;
--- src/repeatedMultiplication.js
+++
@@ -1,8 +0,0 @@
-const repeated_multiplication = (x, y, callback) => {
-    const result = x * y;
-    return setInterval(() => {
-        callback(result);
-    }, 1000)
-};
-
-module.exports = repeated_multiplication;
